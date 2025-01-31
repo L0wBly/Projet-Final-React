134fedeecec0ab895eab45dbf6ef85f22295063e
Arthur <arthurboutry2005@icloud.com>
2025-01-31 09:49:59 +0100

    Ajout d'une date du jour

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import "../../asset/style.css"
 function Hours() {
     const [time, setTime] = useState(new Date());
   
@@ -17,9 +16,15 @@ function Hours() {
         second: '2-digit',
         hour12: false
       });
+
+      let date1 = new Date().toLocaleDateString();
+
+      document.getElementById('p1').innerHTML = date1;
   
     return (
         <div className='heure'>
+          <h1>La date du jour</h1>
+          <p id='p1'></p>
           <p>{formattedTime}</p>
         </div>
     );

8c9668d8d10e4f64a5e5b9c518dbb286d3cee128
Arthur <arthurboutry2005@icloud.com>
2025-01-31 09:26:43 +0100

    Modification fonction recette

--- src/asset/component/Dialog.jsx
+++ src/asset/component/Dialog.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
+import { createElement, useState } from "react";
 
-function Dialog() {
-    // -- states
+function Dialog() { 
+  
     const [dialogOuverte, setDialogOuverte] = useState(false);
   
-    // -- Events
     function gererAffichage() {
       setDialogOuverte((prev) => (!prev));
     }
@@ -14,16 +13,18 @@ function Dialog() {
       <div>
         <button onClick={gererAffichage}>Afficher dialog</button>
   
-        <dialog open={dialogOuverte}>
+        <dialog id="dialog" open={dialogOuverte}>
           <header>
             <h2>Recette du jour</h2>
             <button onClick={gererAffichage}>X</button>
           </header>
           <hr />
-          <img src="https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg" alt="Super alt" style={{ width: '300px' }} />
+          
         </dialog>
       </div>
     );
-  }
+   }
+
+
   
-  export default Dialog;
+export default Dialog;

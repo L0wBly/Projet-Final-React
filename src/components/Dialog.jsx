a05a0f83f5605c03d8f90440bd1e16fc71010443
Arthur BOUTRY <160645017+L0wBly@users.noreply.github.com>
2025-02-07 21:21:51 +0100

    Ajout style Dialog

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -14,7 +14,7 @@ function Dialog() {
         <button onClick={gererAffichage} className="border-2 border-solid pr-4 pl-4 pt-1 pb-1 rounded-[20px] hover:bg-blue-500" >Afficher dialog</button>
   
         <dialog id="dialog" open={dialogOuverte} className="w-full" >
-          <header className="w-1/6">
+          <header className="w-1/6 flex flex-col items-center justify-center bg-violet-500">
             <h2>Recette du jour</h2>
             <button onClick={gererAffichage}>X</button>
           </header>
